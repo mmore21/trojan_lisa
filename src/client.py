fe956f39c18c3a76a23d69b30767eb189d597322
Mason Moreland <mazc@protonmail.ch>
2020-07-24 22:48:30 -0700

    Convert client and server to classes

--- src/client.py
+++ src/client.py
@@ -1,12 +1,17 @@
 import socket
 
-def connect():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('0.0.0.0', 8080))
-    client.send("Message from client")
-    resp = client.recv(4096)
-    client.close()
-    print(resp)
+class Client():
+    def __init__(self):
+        pass
+
+    def connect(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(('0.0.0.0', 8080))
+        client.send("Message from client")
+        resp = client.recv(4096)
+        client.close()
+        print(resp)
 
 if __name__=="__main__":
-   connect() 
+   client = Client()
+   client.connect()
--- src/server.py
+++ src/server.py
@@ -1,21 +1,26 @@
 import socket
 
-def host():
-    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv.bind(('0.0.0.0', 8080))
-    serv.listen(5)
-    while True:
-        conn, addr = serv.accept()
-        resp = ''
+class Server:
+    def __init__(self):
+        pass
+
+    def host(self):
+        serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serv.bind(('0.0.0.0', 8080))
+        serv.listen(5)
         while True:
-            data = conn.recv(4096)
-            if not data:
-                break
-            resp += data
-            print(resp)
-            conn.send("Message from server")
-        conn.close()
-        print("Client disconnected")
+            conn, addr = serv.accept()
+            resp = ''
+            while True:
+                data = conn.recv(4096)
+                if not data:
+                    break
+                resp += data
+                print(resp)
+                conn.send("Message from server")
+            conn.close()
+            print("Client disconnected")
 
 if __name__=="__main__":
-    host()
+    server = Server()
+    server.host()
