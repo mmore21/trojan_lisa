06320f7f6f26e508c7077ab618dabd314362a934
Mason Moreland <mazc@protonmail.ch>
2020-07-25 20:41:57 -0700

    Begin writing out RAT interface

--- src/server.py
+++ src/server.py
@@ -2,7 +2,34 @@ import socket
 
 class Server:
     def __init__(self):
-        pass
+        self.commands = {
+            0: "Exit from RAT interface",
+            1: "Set Mona Lisa as desktop background"
+        }
+        self.run_rat_interface()
+
+
+    def run_rat_interface(self):
+        while True:
+            print("Le Louvre RAT Interface")
+            for command, description in self.commands:
+                print(command, "->", description)
+            
+            command_key = input()
+            # Invalid input
+            if command_key not in self.commands:
+                continue
+            # Exit program
+            elif command_key == 0:
+                return
+            else:
+                self.execute_command(command_key)
+
+    def execute_command(self, command_key):
+        # TODO: Set background to Mona Lisa
+        if command_key == 1:
+            pass
+        
 
     def host(self):
         serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- src/trojan_lisa.py
+++ src/trojan_lisa.py
@@ -1,2 +1,5 @@
+import client
+import server
+
 if __name__ == "__main__":
     pass
